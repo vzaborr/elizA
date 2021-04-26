04e5c5f27b2ef0878aaf7e06e0f2e0ed890ef551
ilia <name.postilga@gmail.com>
2021-04-26 18:48:37 +0300

    meh

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from datetime import datetime
 from datetime import timedelta
 
 import time
+import schedule
+
+import logging
+
+logging.basicConfig()
+schedule_logger = logging.getLogger('schedule')
+schedule_logger.setLevel(level=logging.DEBUG)
+
 
 TOKEN = open("config").read()
 print(TOKEN)
@@ -15,11 +23,19 @@ def reg_text(message):
   now = datetime.now()
   run_at = now + timedelta(seconds=5)
   delay = (run_at - now).total_seconds()
-  
+  """ 
   #threading.Timer(delay, delayed_send, message).start()  
   time.sleep(delay)
   delayed_send(message) 
+  """
+  schedule.every(3).seconds.do(delayed_send, id=message.chat.id)  
+  schedule.run_all()
+  
+  #schedule.clear()
+def delayed_send(id):
+  bot.send_message(id, '<3') 
 
-def delayed_send(message):
-  bot.send_message(message.chat.id, '<3') 
 bot.polling(none_stop=True)
+
+while True:
+ schedule.run_pending()
