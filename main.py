7c1379ade95871942e11575d11b174fa89c215a5
ilia <name.postilga@gmail.com>
2021-04-26 19:24:10 +0300

    meh

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import telebot
-import threading
-from datetime import datetime
-from datetime import timedelta
-
+from threading import Thread
 import time
-import schedule
 
-import logging
+class Sender(Thread):
+  def __init__(self, val, message):
+    Thread.__init__(self)
+    self.val = val
+    self.message = message
 
-logging.basicConfig()
-schedule_logger = logging.getLogger('schedule')
-schedule_logger.setLevel(level=logging.DEBUG)
+  def run(self):
+    time.sleep(self.val)
+    self.delayed_send(self.message.chat.id)
+  
+  def delayed_send(self, id):
+    bot.send_message(id, '<3') 
 
+   
 
 TOKEN = open("config").read()
 print(TOKEN)
@@ -20,22 +24,8 @@ bot = telebot.TeleBot('918854063:AAE8-0Pr5KS_g31hevSpbT-ouF-AJvt0De8')#TOKEN)
 @bot.message_handler(content_types=['text'])
 def reg_text(message):
   bot.send_message(message.chat.id, 'You have to wait..')
-  now = datetime.now()
-  run_at = now + timedelta(seconds=5)
-  delay = (run_at - now).total_seconds()
-  """ 
-  #threading.Timer(delay, delayed_send, message).start()  
-  time.sleep(delay)
-  delayed_send(message) 
-  """
-  schedule.every(3).seconds.do(delayed_send, id=message.chat.id)  
-  schedule.run_all()
   
-  #schedule.clear()
-def delayed_send(id):
-  bot.send_message(id, '<3') 
+  Sender(10, message).start()
 
 bot.polling(none_stop=True)
 
-while True:
- schedule.run_pending()
